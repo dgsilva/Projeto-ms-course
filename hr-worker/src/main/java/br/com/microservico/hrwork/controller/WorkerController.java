743b073cf0f05c1b6162833fb99833b92eb3c7ed
dgsilva <diegorj93@gmail.com>
2021-04-17 19:41:50 -0300

    hr-worker implementation and name project change

--- hr-worker/src/main/java/br/com/microservico/hrwork/controller/WorkerController.java
+++ hr-worker/src/main/java/br/com/microservico/hrwork/controller/WorkerController.java
@@ -3,10 +3,11 @@ package br.com.microservico.hrwork.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 import br.com.microservico.hrwork.entity.Worker;
 import br.com.microservico.hrwork.repository.WorkerRepository;
@@ -22,6 +23,12 @@ public class WorkerController {
 	public List<Worker>buscarTodos(){
 		List<Worker>workerList = workerRepository.findAll();
 		return workerList;
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Worker>findbyId(@PathVariable Long id){
+		Worker obj = workerRepository.findById(id).get();
+		return ResponseEntity.ok(obj);
 	}
 
 }
