142eda925bca0c761e19e6c250828e277abd3950
dgsilva <diegorj93@gmail.com>
2021-04-18 14:54:20 -0300

    Ribbon load balancing impletation

--- hr-payroll/src/main/java/br/com/microservico/hrpayroll/HrPayrollApplication.java
+++ hr-payroll/src/main/java/br/com/microservico/hrpayroll/HrPayrollApplication.java
@@ -2,9 +2,11 @@ package br.com.microservico.hrpayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 @EnableFeignClients
 @SpringBootApplication
+@RibbonClient(name="hr-worker")
 public class HrPayrollApplication {
 
 	public static void main(String[] args) {
