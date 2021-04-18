11a9a2aecac823c6b4e2e13864b7520b792fd2db
dgsilva <diegorj93@gmail.com>
2021-04-18 15:09:31 -0300

    Project hr-eureka-server created

---
+++ hr-eureka-server/src/test/java/br/com/microservico/hreureka/HrEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microservico.hreureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
