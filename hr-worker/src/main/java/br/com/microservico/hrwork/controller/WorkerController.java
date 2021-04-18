1722f10981a18ae972bc0d3324cfbba1bb2bbf6c
dgsilva <diegorj93@gmail.com>
2021-04-18 14:59:44 -0300

    Ribbon load impletation workerController

--- hr-worker/src/main/java/br/com/microservico/hrwork/controller/WorkerController.java
+++ hr-worker/src/main/java/br/com/microservico/hrwork/controller/WorkerController.java
@@ -2,7 +2,10 @@ package br.com.microservico.hrwork.controller;
 
 import java.util.List;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,8 +19,13 @@ import br.com.microservico.hrwork.repository.WorkerRepository;
 @RequestMapping("/workers")
 public class WorkerController {
 	
+	private static Logger logger = LoggerFactory.getLogger(WorkerController.class);
+	
 	@Autowired
 	private WorkerRepository workerRepository;
+	
+	@Autowired
+	private Environment env;
 	
 	@GetMapping
 	public List<Worker>buscarTodos(){
@@ -27,8 +35,12 @@ public class WorkerController {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker>findbyId(@PathVariable Long id){
+		logger.info("PORT = " + env.getProperty("local.server.port"));
+		
 		Worker obj = workerRepository.findById(id).get();
 		return ResponseEntity.ok(obj);
 	}
+	
+	
 
 }
